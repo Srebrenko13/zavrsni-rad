a90f30cd124ff0b82a13f5d93b990d942f5f5b37
Renato Malbašić <renato.malbasic@fer.hr>
2024-05-16 20:24:49 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <p>This is base file of your project!</p>
         </p>
         <a
           className="App-link"
